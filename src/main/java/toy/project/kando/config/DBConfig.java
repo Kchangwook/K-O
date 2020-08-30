dfbff3d54c893d9766192ac9b697d39110f91e29
kchangwook <kchangwook@nts-corp.com>
2020-08-30 17:39:34 +0900

    초기 설정 완료

---
+++ src/main/java/toy/project/kando/config/DBConfig.java
@@ -0,0 +1,47 @@
+package toy.project.kando.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@PropertySource("classpath:/db.properties")
+public class DBConfig {
+	private final String driverName;
+	private final String url;
+	private final String username;
+	private final String password;
+
+	public DBConfig(
+		@Value("${datasource.driver}") String driverName,
+		@Value("${datasource.url}") String url,
+		@Value("${datasource.username}") String username,
+		@Value("${datasource.password}") String password) {
+		this.driverName = driverName;
+		this.url = url;
+		this.username = username;
+		this.password = password;
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setDriverClassName(driverName);
+		dataSource.setJdbcUrl(url);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+		return dataSource;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(DataSource dataSource) {
+		return new DataSourceTransactionManager(dataSource);
+	}
+}
---
+++ src/main/java/toy/project/kando/config/WebInitializer.java
@@ -0,0 +1,17 @@
+package toy.project.kando.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {ApplicationConfig.class};
+	}
+
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {WebConfig.class};
+	}
+
+	protected String[] getServletMappings() {
+		return new String[] {"/*"};
+	}
+}
