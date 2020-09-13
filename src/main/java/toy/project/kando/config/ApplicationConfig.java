9fb268facc446a01658a16ccb36723a3ae2f6eae
kchangwook <kchangwook@nts-corp.com>
2020-09-14 00:03:58 +0900

    메인 페이지 완성

--- src/main/java/toy/project/kando/config/ApplicationConfig.java
+++ src/main/java/toy/project/kando/config/ApplicationConfig.java
@@ -1,11 +1,10 @@
 package toy.project.kando.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import(value = {DBConfig.class, MybatisConfig.class})
-@ComponentScan("toy.project.kando")
+// @ComponentScan(basePackages = {"toy.project.kando"})
 public class ApplicationConfig {
 }
--- src/main/java/toy/project/kando/config/WebConfig.java
+++ src/main/java/toy/project/kando/config/WebConfig.java
@@ -1,12 +1,37 @@
 package toy.project.kando.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"toy.project.kando"})
 public class WebConfig implements WebMvcConfigurer {
+	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("main");
+	}
 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+		registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+		registry.addResourceHandler("/vendor/**").addResourceLocations("classpath:/static/vendor/");
+		registry.addResourceHandler("/html/**").addResourceLocations("classpath:/static/html/");
+		registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
+	}
+
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+		internalResourceViewResolver.setPrefix("/html/");
+		internalResourceViewResolver.setSuffix(".html");
+		return internalResourceViewResolver;
 	}
 }
